63417e9047be3f725307e180bdaad39f02b2a7ad
unknown <shymechko.m.a@gmail.com>
2019-11-01 14:31:53 +0200

    commit

--- Lesson_3/task_2/index.js
+++ Lesson_3/task_2/index.js
@@ -1,4 +1,4 @@
-console.log('17 to string is ' + String(-17));
+console.log('17 to string is ' + String(17));
 console.log('-17.17 to string is ' + String(-17.17));
 console.log('false to string is ' + String(false));
 console.log('null to string is ' + String(null));
