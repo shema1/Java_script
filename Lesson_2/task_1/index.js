98f910d415069197896351179ae24ccc9c899893
unknown <shymechko.m.a@gmail.com>
2019-11-01 09:34:27 +0200

    commit

---
+++ Lesson_2/task_1/index.js
@@ -0,0 +1,8 @@
+const userName = 'John';
+const greeting = "I'm John";
+const profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+let super_Number = 17;
