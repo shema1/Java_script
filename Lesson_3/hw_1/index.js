df31dacc496a594921aa8e4702452c4b87c23c3c
unknown <shymechko.m.a@gmail.com>
2019-11-01 15:19:13 +0200

    commit

--- Lesson_3/hw_1/index.js
+++ Lesson_3/hw_1/index.js
@@ -1,8 +1,23 @@
-let a = 4;
-let b = 2;
-console.log("a: " + a);
-console.log("b: " + "[" + b + "]");
-console.log(a - b);
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
+/* Этот код менять не нужно */
+const defaultUserName = 'anonymus';
+const userName = 'John';
+const anotherUser = null;
+const isAdmin = false;
+const message = 'text';
+const anotherMessage = 'text as well';
+const isLoggedIn = Boolean(userName);
+
+/* В коде ниже нужно использовать правильные операторы после знака присваивания */
+const activeUser = anotherUser || defaultUserName;
+const hasAccess = isAdmin || isLoggedIn;
+const isTruthy = message != anotherMessage;
+const isFalsy = !isLoggedIn;
+const isNotTrue = typeof message == 'number';
+const isTrue = typeof message != 'boolean';
+
+console.log(activeUser);
+console.log(hasAccess);
+console.log(isTruthy);
+console.log(isFalsy);
+console.log(isNotTrue);
+console.log(isTrue);
