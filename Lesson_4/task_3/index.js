e71f764e030e3d522069af7b8731e5de520372f0
unknown <shymechko.m.a@gmail.com>
2019-11-01 17:43:36 +0200

    c

--- Lesson_4/task_3/index.js
+++ Lesson_4/task_3/index.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 5; i++) {
-    console.log('=> ' + i + ' <=');
+    console.log('=>' + i + '<=')
     for (let j = 1; j <= 10; j++) {
         console.log(i + ' x ' + j + '=' + i * j)
 
