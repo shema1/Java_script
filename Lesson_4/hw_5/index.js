59572c954942fe56d43b1bc9dcd443677418a995
unknown <shymechko.m.a@gmail.com>
2019-11-02 12:18:26 +0200

    commit

--- Lesson_4/hw_5/index.js
+++ Lesson_4/hw_5/index.js
@@ -1,7 +1,15 @@
-const n = 20;
-let count = 1;
-let sum = '';
-while (count <= n) {
-    console.log(sum += count);
-    count++;
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++) {
+    sum += i;
+}
+
+let a = sum % 1234;
+let b = sum / 1234;
+
+
+if (a > b) {
+    console.log('true')
+} else {
+    console.log('false')
 }
