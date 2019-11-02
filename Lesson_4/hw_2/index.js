a02934563b56fa0f2dfb0c5293083f944caf46ee
unknown <shymechko.m.a@gmail.com>
2019-11-02 11:18:31 +0200

    commit

--- Lesson_4/hw_2/index.js
+++ Lesson_4/hw_2/index.js
@@ -4,15 +4,14 @@ let result = 0;
 
 
 for (let i = m; i <= n; i++) {
-    if (i % 2 == 0 && i % 4 !== 0) {
+    if (i % 2 === 0 && i % 4 !== 0) {
         result += i;
-    } else if (i % 3 == 0) {
+    } else if (i % 3 === 0) {
         result -= i;
-    } else if (i % 4 == 0) {
+    } else if (i % 4 === 0) {
         result *= i;
-    } else if (i % 5 == 0) {
+    }
+    if (i % 5 === 0) {
         console.log(i)
     }
 }
-
-console.log(result);
