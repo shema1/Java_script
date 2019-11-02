5d61cb9d2c436068e130abf243feb16d17519896
unknown <shymechko.m.a@gmail.com>
2019-11-02 11:50:05 +0200

    commit

--- Lesson_4/hw_3/index.js
+++ Lesson_4/hw_3/index.js
@@ -1,7 +1,21 @@
-const n = 20;
-let count = 1;
-let sum = '';
-while (count <= n) {
-    console.log(sum += count);
-    count++;
+let sum = 0;
+let a = 50;
+let b = 100;
+
+
+for (let i = a; i < b; i++) {
+
+    if (i % 2 !== 0) {
+        console.log('Found')
+        sum += i;
+    }
+
+}
+
+if (sum * 5 > 5000) {
+    console.log("Bigger");
+} else if (sum * 5 < 5000) {
+    console.log("Smaller");
+} else {
+    console.log("equal");
 }
