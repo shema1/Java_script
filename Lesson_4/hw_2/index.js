c7100ac51178501ec8caebc536ab5581e96cd016
unknown <shymechko.m.a@gmail.com>
2019-11-02 11:14:13 +0200

    commit

--- Lesson_4/hw_2/index.js
+++ Lesson_4/hw_2/index.js
@@ -3,13 +3,13 @@ const n = 20;
 let result = 0;
 
 
-for (let i = m; i < n; i++) {
+for (let i = m; i <= n; i++) {
     if (i % 2 == 0 && i % 4 !== 0) {
         result += i;
     } else if (i % 3 == 0) {
         result -= i;
     } else if (i % 4 == 0) {
-        result *= count;
+        result *= i;
     } else if (i % 5 == 0) {
         console.log(i)
     }
