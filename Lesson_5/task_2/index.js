4695702ed608473d3f4a56dfbc95d6a2bc03e0c1
unknown <shymechko.m.a@gmail.com>
2019-11-04 18:47:51 +0200

    commit

--- Lesson_5/task_2/index.js
+++ Lesson_5/task_2/index.js
@@ -1,12 +1,4 @@
-let m = 10;
-let n = 20;
-let count = m;
-let result = 1;
-
-do {
-    if (count % 2 == 1) {
-        result *= count;
-    }
-    count++
-} while (count < n);
-console.log(result);
+function sayHi(name) {
+    const greeting = 'Hi, ' + name;
+    console.log(greeting);
+}
