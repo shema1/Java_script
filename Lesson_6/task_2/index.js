efea0215c7a9a8ffa0fd0131846c272546d498cc
unknown <shymechko.m.a@gmail.com>
2019-11-04 21:05:05 +0200

    commit

--- Lesson_6/task_2/index.js
+++ Lesson_6/task_2/index.js
@@ -2,7 +2,7 @@ const numbers = [10, 45, 99, 1, 65, 7, 12, 23];
 
 function getSum(arr) {
     let sum = 0;
-    if (!Array.isArray) {
+    if (!Array.isArray(arr)) {
         return null
     } else {
         for (let i = 0; i < arr.length; i++) {
