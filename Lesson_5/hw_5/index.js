06777b8911fcc38a3fbf8a46456747fc4f2c6285
unknown <shymechko.m.a@gmail.com>
2019-11-04 19:58:16 +0200

    commit

--- Lesson_5/hw_5/index.js
+++ Lesson_5/hw_5/index.js
@@ -3,6 +3,8 @@ function findDivCount(a, b, n) {
     for (let i = a; i <= b; i++) {
         if (i % n === 0) {
             console.log(i);
+        } else {
+            return 0;
         }
 
     }
