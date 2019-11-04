5a084e493c75926f8eff67144f35e0aedccdbef1
unknown <shymechko.m.a@gmail.com>
2019-11-04 22:20:56 +0200

    commit

--- Lesson_6/hw_3/index.js
+++ Lesson_6/hw_3/index.js
@@ -1,7 +1,23 @@
-function increaser(a, index) {
-    if (a > index) {
-        a += index;
-        return a;
+const numbers = [8, 2, 7, 6, 4, 1, 58];
+
+function sortAsc(arr) {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j + 1] < arr[j]) {
+                let max = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = max;
+            }
+
+        }
+
     }
-    return a;
+
+    return arr;
+
 }
+
+
+
+console.log(sortAsc(numbers));
