0444b7fb3fcdc7ffc7e6056290173eccf95835db
unknown <shymechko.m.a@gmail.com>
2019-11-04 19:33:32 +0200

    commit

--- Lesson_5/hw_3/index.js
+++ Lesson_5/hw_3/index.js
@@ -1,18 +1,6 @@
-let sum = 0;
-
-
-
-for (let i = 0; i <= 1000; i++) {
-
-    if (i % 2 != 0) {
-        console.log('Found');
-        sum += i;
+function increaser(a, index) {
+    if (a > index) {
+        a *= index;
     }
-
-}
-
-if (sum * 5 > 5000) {
-    console.log("Bigger");
-} else {
-    console.log("Smaller or equal");
+    return a;
 }
