acc9551eb9c63d19b7be87c1ff8c9a3a8d5b8466
unknown <shymechko.m.a@gmail.com>
2019-11-04 19:57:00 +0200

    commit

--- Lesson_5/hw_5/index.js
+++ Lesson_5/hw_5/index.js
@@ -1,10 +1,11 @@
 function findDivCount(a, b, n) {
+
     for (let i = a; i <= b; i++) {
-        if (i % n == 0) {
-            console.log(a);
+        if (i % n === 0) {
+            console.log(i);
         }
 
     }
 }
 
-findDivCount(10, 30, 3);
+findDivCount(10, 40, 3);
