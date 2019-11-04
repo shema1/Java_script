b84e3eafe4d0e5e1778dd90180b8a568f14a7b32
unknown <shymechko.m.a@gmail.com>
2019-11-04 20:57:59 +0200

    commit

--- Lesson_6/task_1/index.js
+++ Lesson_6/task_1/index.js
@@ -1,10 +1,11 @@
 function getArrayBounds(arr) {
-    if (Array.isArray) {
-        return [arr.length, arr[0], arr[arr.length - 1]];
-    }
-    return null;
-}
+    if (!Array.isArray) {
 
+        return null;
+    }
+    return [arr.length, arr[0], arr[arr.length - 1]];
+
+}
 const numbers = [1, 2, 4];
 
 const arrayBound = getArrayBounds(numbers);
