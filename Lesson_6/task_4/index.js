6e0391f6b8181b4beb9a8b65eb5ef9cf536bb433
unknown <shymechko.m.a@gmail.com>
2019-11-04 21:42:01 +0200

    commit

--- Lesson_6/task_4/index.js
+++ Lesson_6/task_4/index.js
@@ -1,1 +1,8 @@
-const square = (num) => num * num;
+let numbers = [1, 32, 25, 78, 1, 0, 5, 8, 5, 88]
+
+function swap(arr) {
+    const [start, ...rest] = arr;
+    return [...rest, start];
+}
+
+console.log(swap(numbers));
