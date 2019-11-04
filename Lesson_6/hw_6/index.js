d7eac25df9528ce8841dd807d4cc8d3c2d823c51
unknown <shymechko.m.a@gmail.com>
2019-11-04 23:16:00 +0200

    commit

--- Lesson_6/hw_6/index.js
+++ Lesson_6/hw_6/index.js
@@ -5,13 +5,12 @@ let balances = [3200, 11002, 300, 6824]
 function withdraw(clients, balances, client, amount) {
 
     let indexUser = clients.indexOf(client);
-    if (balances[indexUser] >= amount) {
-        balances[indexUser] -= amount
+    if (balances[indexUser] - amount >= 0) {
+
+        return balances[indexUser] -= amount
     } else {
         return -1;
     }
-
-    return clients, balances;
 }
 
 withdraw(clients, balances, "Mykola", 100);
