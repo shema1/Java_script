24a7aa5f232e4f5fce5407d5ea3fa34f211638d7
unknown <shymechko.m.a@gmail.com>
2019-11-04 22:24:11 +0200

    commit

--- Lesson_6/hw_3/index.js
+++ Lesson_6/hw_3/index.js
@@ -18,6 +18,25 @@ function sortAsc(arr) {
 
 }
 
+function sortDesc(arr) {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j + 1] > arr[j]) {
+                let max = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = max;
+            }
+
+        }
+
+    }
+
+    return arr;
+
+}
+
 
 
 console.log(sortAsc(numbers));
+console.log(sortDesc(numbers));
