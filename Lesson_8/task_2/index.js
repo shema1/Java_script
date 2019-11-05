7ffbb1cb99fd6f8f5777961790b524e98f002227
unknown <shymechko.m.a@gmail.com>
2019-11-05 16:47:33 +0200

    commit

--- Lesson_8/task_2/index.js
+++ Lesson_8/task_2/index.js
@@ -1,1 +1,17 @@
-const sortDesc = numbers => numbers.slice().sort((a, b) => b - a);
+const user = {
+    name: 'Tom',
+    age: 17
+}
+
+
+
+const concatProps = obj => {
+    const arr = [];
+    for (const key in obj) {
+        arr.push(obj[key])
+    }
+
+    return arr;
+}
+
+console.log(concatProps(user));
