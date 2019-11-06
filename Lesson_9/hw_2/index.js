b181dadb70ad0ebdc4fc85c12b5816c310494824
unknown <shymechko.m.a@gmail.com>
2019-11-06 19:21:40 +0200

    commit

--- Lesson_9/hw_2/index.js
+++ Lesson_9/hw_2/index.js
@@ -1,7 +1,3 @@
-function getPeople(obj) {
-
-    const arr = Object.values(obj).map(i => i.map(i => i.name))
-        .flat();
-
-    return arr
-}
+const getPeople = obj =>
+    Object.values(obj).map(i => i.map(i => i.name))
+    .flat();
