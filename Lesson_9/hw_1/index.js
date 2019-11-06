b60f49a299b850cabef50ca274b015ac1b000546
unknown <shymechko.m.a@gmail.com>
2019-11-06 17:22:23 +0200

    commit

--- Lesson_9/hw_1/index.js
+++ Lesson_9/hw_1/index.js
@@ -1,13 +1,27 @@
-function pickProps(obj, arr) {
-    let testObj = {};
-
-    for (let i of arr) {
-        for (const key in obj) {
-            if (i == key) {
-                testObj[key] = obj[key]
-            }
-        }
-
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
     }
-    return testObj
+};
+
+// let arr = Object.entries(customers);
+// let test = arr.map(i => i[1].id = i[0]);
+// console.log(arr)
+// console.log(test)
+
+function getCustomersList(obj) {
+    let arr = Object.entries(obj);
+    let test = [];
+
+    arr.map(i => i.id = i);
+    arr.forEach(i => test.push(i[1]));
+
+    return test.sort((a, b) => a.age > b.age ? 1 : -1)
 }
+
+console.log(getCustomersList(customers));
