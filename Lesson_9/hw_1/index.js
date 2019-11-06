e978bcc2f31017e8daf680f6ec00c8efefea261c
unknown <shymechko.m.a@gmail.com>
2019-11-06 19:48:56 +0200

    commit

--- Lesson_9/hw_1/index.js
+++ Lesson_9/hw_1/index.js
@@ -11,23 +11,14 @@ const customers = {
 
 
 function getCustomersList(obj) {
-    let arr = Object.entries(obj);
+    const copy = JSON.parse(JSON.stringify(customers));
+    let arr = Object.entries(copy);
     let test = [];
 
-
-    for (let i = 0; i < arr.length; i++) {
-        arr.id = test[i];
-    }
+    arr.map(i => i[1].id = i[0]);
     arr.forEach(i => test.push(i[1]));
 
     return test.sort((a, b) => a.age > b.age ? 1 : -1)
 }
 
 console.log(getCustomersList(customers));
-
-// let arr = Object.values(customers);
-
-// let arr2 = Object.entries(customers);
-
-// console.log(arr)
-// console.log(arr2)
