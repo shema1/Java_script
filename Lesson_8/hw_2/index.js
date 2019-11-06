b5f14b59f1f27bce4218f684455c8ed59a212f13
unknown <shymechko.m.a@gmail.com>
2019-11-06 09:49:37 +0200

    commit

--- Lesson_8/hw_2/index.js
+++ Lesson_8/hw_2/index.js
@@ -1,7 +1,7 @@
 const getAdults = obj => {
     let testObj = {};
     for (const key in obj) {
-        if (obj[key >= 18]) {
+        if (obj[key] >= 18) {
             testObj[key] = obj[key]
         }
     }
