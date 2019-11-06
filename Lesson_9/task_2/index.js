fb29a9cfa0c2b35ac2b8c74966a4759e2b0a3a4c
unknown <shymechko.m.a@gmail.com>
2019-11-06 16:06:48 +0200

    commit

--- Lesson_9/task_2/index.js
+++ Lesson_9/task_2/index.js
@@ -1,17 +1,2 @@
-const user = {
-    name: 'Tom',
-    age: 17
-}
-
-
-
-const concatProps = obj => {
-    const arr = [];
-    for (const key in obj) {
-        arr.push(obj[key])
-    }
-
-    return arr;
-}
-
-console.log(concatProps(user));
+const copyObj = obj => Object
+    .assign({}, obj);
