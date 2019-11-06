348d791bbc28e309cb95a5e3e68c92e823902a95
unknown <shymechko.m.a@gmail.com>
2019-11-06 19:25:02 +0200

    commit

--- Lesson_9/hw_2/index.js
+++ Lesson_9/hw_2/index.js
@@ -1,30 +1,6 @@
-rooms = {
-    room1: [
-        { name: 'room1 name1' },
-        { name: 'room1 name2' },
-        { name: 'room1 name3' },
-        { name: 'room1 name4' },
-
-    ],
-    room2: [
-        { name: 'room2 name1' },
-
-    ],
-    room3: [
-        { name: 'room3 name1' },
-        { name: 'room3 name2' },
-        { name: 'room3 name3' },
-
-    ],
-
-};
-
-
-function getPeople(obj) {
+const getPeople = obj => {
 
     const arr = Object.values(obj).map(i => i.map(i => i.name))
 
     return arr.concat.apply([], arr)
 }
-
-console.log(getPeople(rooms));
