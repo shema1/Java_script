1ed8c131661317224f7265c0015802e0e79eaca6
unknown <shymechko.m.a@gmail.com>
2019-11-07 20:16:26 +0200

    commit

--- Lesson_11/hw_1/index.js
+++ Lesson_11/hw_1/index.js
@@ -1,8 +1,27 @@
-function getTotalPrice(arr) {
-    let test = 0;
-    test = arr.reduce((acc, b) => {
-        return acc += b
-    });
+const splitString = (text, len) => {
+    const strArr = [];
+    let startPosition = 0;
+    if (typeof text !== "string") {
+        return null;
+    }
+    while (true) {
 
-    return '$' + Math.floor(test * 100) / 100;
-}
+        let chunk = text.substr(startPosition, len);
+        if (chunk.length === 0) {
+            break
+        }
+
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += len;
+        if (chunk.length < len) {
+            strArr.push(chunk + '.'.repeat(len - chunk.length))
+        } else {
+            strArr.push
+        }
+    }
+    return strArr.join('\n');
+};
+
+
+
+console.log(splitString('asakuierneydebhcnsagduyegfrbsjuaiquieryrtbsncfdgcfgdasdsavregbrbvsds', 3));
