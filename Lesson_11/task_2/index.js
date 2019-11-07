50fb5ea316c57a4ade89fa6b4390b1f89951f897
unknown <shymechko.m.a@gmail.com>
2019-11-07 19:38:28 +0200

    commit

--- Lesson_11/task_2/index.js
+++ Lesson_11/task_2/index.js
@@ -1,16 +1,8 @@
-const getParsedIntegers = arr => arr
-    .map(i => Number.parseInt(i));
+const sortContacts = contacts => {
+    const result = contacts
+        .sort((a, b) => {
+            return a.name.localeCompare(b.name)
+        });
 
-
-const getParsedIntegersV2 = arr => arr
-    .map(i => parseInt(i));
-
-const getParsedFloats = arr => arr
-    .map(i => Number.parseFloat(i));
-
-const getParsedFloatsV2 = arr => arr
-    .map(i => parseFloat(i));
-
-const arr = [14, 2.28, , null, , 'text', '1734text', NaN];
-
-console.log(arr);
+    return result;
+}
