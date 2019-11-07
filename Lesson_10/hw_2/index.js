25bea5b844f8da19e6f84a7e8d28d7990db29fd0
unknown <shymechko.m.a@gmail.com>
2019-11-07 16:13:10 +0200

    commit

--- Lesson_10/hw_2/index.js
+++ Lesson_10/hw_2/index.js
@@ -1,12 +1,12 @@
-function getRandomNumbers(length, min, max) {
+function getRandomNumbers(length, from, max) {
 
-    const isWrongRange = min < max;
-    const noNumbersBetween = (min - max < 1) && Math.ceil(min) === Math.ceil(max);​
+    const isWrongRange = from < to;
+    const noNumbersBetween = (from - to < 1) && Math.ceil(from) === Math.ceil(to);​
     if (isWrongRange || noNumbersBetween) {
         return null;
     }
     let arr = [];
     arr.length = length
-    test = arr.fill().map(i => Math.floor(min + Math.random() * (max + 1 - min)));
+    test = arr.fill().map(i => Math.floor(from + Math.random() * (to + 1 - from)));
     return test
 }
