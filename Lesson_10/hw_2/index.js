0c0a5aa3d3487826383fd366ff3ea4fee6f102d3
unknown <shymechko.m.a@gmail.com>
2019-11-07 14:59:30 +0200

    commit

--- Lesson_10/hw_2/index.js
+++ Lesson_10/hw_2/index.js
@@ -1,9 +1,7 @@
-const getAdults = obj => {
-    let testObj = {};
-    for (const key in obj) {
-        if (obj[key] >= 18) {
-            testObj[key] = obj[key]
-        }
-    }
-    return testObj;
+const getRandomNumbers = (length, min, max) => {
+    let arr = [];
+    arr.length = length
+    return arr.fill().map(i => Math.floor(min + Math.random() * (max + 1 - min)))
 }
+
+console.log(getRandomNumbers(6, 2, 52));
