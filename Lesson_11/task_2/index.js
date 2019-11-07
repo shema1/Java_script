810d91dee2e6633c226ac9f7dd753c61d0942733
unknown <shymechko.m.a@gmail.com>
2019-11-07 19:42:32 +0200

    commit

--- Lesson_11/task_2/index.js
+++ Lesson_11/task_2/index.js
@@ -1,9 +1,11 @@
 const sortContacts = contacts => {
-    if (!Array.isArray(contacts))
-        const result = contacts
-            .sort((a, b) => {
-                return b.name.localeCompare(a.name)
-            });
+    if (!Array.isArray(contacts)) {
+        return null;
+    }
+    const result = contacts
+        .sort((a, b) => {
+            return b.name.localeCompare(a.name)
+        });
 
     return result;
 }
