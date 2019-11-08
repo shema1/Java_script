2c02ff701f2879778fe51158df92072ad35996b3
unknown <shymechko.m.a@gmail.com>
2019-11-08 13:27:06 +0200

    commit

--- Lesson_12/hw_2/index.js
+++ Lesson_12/hw_2/index.js
@@ -2,7 +2,7 @@ let test = [' 1.9 ', '16.4', 17, ' 1 dollar '];
 
 const cleanTransactionsList = arr => {
 
-    res = arr.filter(i => typeof + i === 'number').map(i => Number(i).toFixed(2));
+    res = arr.filter(i => typeof + i === 'number').map(i => '$' + Number(i).toFixed(2));
     return res;
 
 }
