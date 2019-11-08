6f1278c4099760c35b58bdccb7e728a1abe4e0c1
unknown <shymechko.m.a@gmail.com>
2019-11-08 13:43:19 +0200

    commit

--- Lesson_12/hw_2/index.js
+++ Lesson_12/hw_2/index.js
@@ -4,3 +4,5 @@ const cleanTransactionsList = arr => {
     return arr.filter(i => !isNaN(i)).map(i => '$' + Number(i).toFixed(2));
 
 }
+
+console.log(cleanTransactionsList([' 1.9 ', '16.4', 17, ' 1 dollar ', ' 1.9 ', '16.4', 17, ' 1 dollar ', ' 1.9 ', '16.4', 17, ' 1 dollar ']));
