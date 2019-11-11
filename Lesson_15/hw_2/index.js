aef7b26d7c010fcbe5d60175fb0a57760632c6d6
unknown <shymechko.m.a@gmail.com>
2019-11-11 21:00:29 +0200

    commit

--- Lesson_15/hw_2/index.js
+++ Lesson_15/hw_2/index.js
@@ -1,19 +1,35 @@
-let calc = 0;
+function createLogger() {
+    let logInfo = [];
+    return {
+        warn: function(info) {
+            logInfo.push({
+                message: info,
+                dateTime: new Date(),
+                type: 'warn',
+            })
+        },
+        error: function(info) {
+            logInfo.push({
+                message: info,
+                dateTime: new Date(),
+                type: 'error',
+            })
+        },
+        log: function(info) {
+            logInfo.push({
+                message: info,
+                dateTime: new Date(),
+                type: 'log',
+            })
+        },
 
-function add(num) {
-    calc += num;
+        getRecords: function(ins) {
+            if (ins === undefined) {
+                return logInfo
+            }
+
+            return logInfo.filter(i => i.type === ins).sort((a, b) => b.dateTime.getTime() - a.dateTime.getTime());
+        },
+    }
 }
-
-function decrease(num) {
-    calc -= num;
-}
-
-function reset() {
-    calc = 0;
-}
-
-function getMemo() {
-    return calc;
-}
-
-export { add, decrease, reset, getMemo }
+export { createLogger };
