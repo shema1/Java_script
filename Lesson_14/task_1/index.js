7e24f1ccaf85a8c5ad684fa1b68336f8d8dfd197
unknown <shymechko.m.a@gmail.com>
2019-11-11 17:36:03 +0200

    commit

---
+++ Lesson_14/task_1/index.js
@@ -0,0 +1,9 @@
+const message = 'Just lern it!';
+
+const sendMessage = (name) => {
+
+    const sender = 'Grome code';
+    console.log(`${name}, ${message}. You ${sender}`);
+}
+
+sendMessage('Tom');
---
+++ Lesson_14/task_2/index.js
@@ -0,0 +1,21 @@
+const calc = expression => {
+    const [a, operator, b] = expression.split(' ');
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = Number(a) + Number(b);
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+
+    return expression + ' = ' + result;
+}
