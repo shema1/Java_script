27d92051cbb1b057e122e056432ad9f05f17829f
unknown <shymechko.m.a@gmail.com>
2019-11-25 14:15:25 +0200

    commit

--- Lesson_17/hw_1/index.js
+++ Lesson_17/hw_1/index.js
@@ -5,13 +5,14 @@ const user = {
         console.log(this.firstName + ' ' + this.lastName)
         return this.firstName + ' ' + this.lastName;
     },
-    setFullName(name, ln) {
-        this.firstName = name;
-        this.lastName = ln;
+    setFullName(fullName) {
+        let array = fullName.split(' ');
+        this.firstName = array[0];
+        this.lastName = array[1];
     }
 }
 
-// user.getFullName();
-// user.setFullName('Tom', 'Bom')
-// user.getFullName();
-export { user };
+user.getFullName();
+user.setFullName('Tom Bom')
+user.getFullName();
+// export { user };
