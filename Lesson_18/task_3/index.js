c212186b8286f3eebf637c3deba8aab6fed7c140
unknown <shymechko.m.a@gmail.com>
2019-11-25 20:08:27 +0200

    commit

--- Lesson_18/task_3/index.js
+++ Lesson_18/task_3/index.js
@@ -1,9 +1,9 @@
-function argSum() {
+function sumOfSquares() {
     return [...arguments].reduce((acc, elem) => {
-        return acc + elem
+        return acc + elem * elem
     }, 0);
 }
 
-console.log(argSum(1, 2, 3, 4))
+console.log(sumOfSquares(1, 2, 3, 4))
 
-export { argSum }
+export { sumOfSquares }
