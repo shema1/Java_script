fb7e7b6e83a63d0a3ef48192baab31199dfe7f0b
unknown <shymechko.m.a@gmail.com>
2019-11-25 17:06:05 +0200

    commit

--- Lesson_17/hw_2/index.js
+++ Lesson_17/hw_2/index.js
@@ -1,35 +1,35 @@
 let timer = {
-    secondsPassed: 0,
-    minsPassed: 0,
-    startTimer() {
-        let step = setInterval(() => {
-            this.secondsPassed += 5
-            if (this.secondsPassed === 60) {
-                this.secondsPassed = 0;
-                this.minsPassed++
+        secondsPassed: 0,
+        minsPassed: 0,
+        startTimer() {
+            let step = setInterval(() => {
+                this.secondsPassed += 5
+                if (this.secondsPassed === 60) {
+                    this.secondsPassed = 0;
+                    this.minsPassed++
+                }
+            }, 5000);
+
+        },
+        stopTimer() {
+            clearInterval(this.startTimer)
+        },
+        getTime() {
+            if (this.secondsPassed < 10) {
+                return (`${this.minsPassed}:0${this.secondsPassed}`)
             }
-        }, 5000);
-
-    },
-    stopTimer() {
-        clearInterval(this.startTimer)
-    },
-    getTime() {
-        if (this.secondsPassed < 10) {
-            return (`${this.minsPassed}:0${this.secondsPassed}`)
+            if (this.secondsPassed > 10) {
+                return (`${this.minsPassed}:${this.secondsPassed}`)
+            }
+        },
+        resetTimer() {
+            this.secondsPassed = 0;
+            this.minsPassed = 0;
         }
-        if (this.secondsPassed > 10) {
-            return (`${this.minsPassed}:${this.secondsPassed}`)
-        }
-    },
-    resetTimer() {
-        this.secondsPassed = 0;
-        this.minsPassed = 0;
     }
-}
-timer.startTimer();
-timer.getTime();
-timer.stopTimer();
-timer.startTimer();
+    // timer.startTimer();
+    // timer.getTime();
+    // timer.stopTimer();
+    // timer.startTimer();
 
-// export { timer }
+export { timer }
