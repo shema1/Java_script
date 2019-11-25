4086fdde53ba0cdd443eff2d4830bd066e7a5cab
unknown <shymechko.m.a@gmail.com>
2019-11-25 18:23:42 +0200

    commit

--- Lesson_17/hw_3/index.js
+++ Lesson_17/hw_3/index.js
@@ -1,5 +1,9 @@
 const bind = (func, context) => {
-    return func.apply(context, arguments);
+    let args = [].slice.call(arguments)
+    return function() {
+        let test = [].slice.call(arguments)
+        return func.apply(context, args.concat(test))
+    }
 };
 
 export { bind };
