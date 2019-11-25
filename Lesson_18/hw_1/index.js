334063fe1f3c7d28857cb04291e0403365c7aca0
unknown <shymechko.m.a@gmail.com>
2019-11-25 19:31:28 +0200

    commit

---
+++ Lesson_18/hw_1/index.js
@@ -0,0 +1,18 @@
+const user = {
+    firstName: '',
+    lastName: '',
+    getFullName() {
+        console.log(`${this.firstName} ${this.lastName}`)
+        return `${this.firstName} ${this.lastName}`
+    },
+    setFullName(fullName) {
+        let array = fullName.split(' ');
+        this.firstName = array[0];
+        this.lastName = array[1];
+    }
+}
+
+user.getFullName();
+// user.setFullName('Tom Bom')
+// user.getFullName();
+export { user };
---
+++ Lesson_18/task_1/index.js
@@ -0,0 +1,18 @@
+const event = {
+    message: 'Welcome to the party!',
+    guests: [
+        { name: 'Tom', age: 21, email: 'a@server.com' },
+        { name: 'Bob', age: 15, email: 'b@server.com' }
+    ],
+    getInvitations() {
+        return this.guests
+            .filter(i => i.age >= 18)
+            .map(({ name, email }) => ({
+                email: `${email}`,
+                message: `Dear ${name}! ${this.message}`
+            }));
+    }
+}
+
+console.log(event.getInvitations());
+export { event };
