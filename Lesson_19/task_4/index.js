c67f31b12b9c95a27d135a05d1a3deec084fff9f
unknown <shymechko.m.a@gmail.com>
2019-11-26 18:59:40 +0200

    commit

--- Lesson_19/task_4/index.js
+++ Lesson_19/task_4/index.js
@@ -8,19 +8,20 @@ User.prototype.sayHi = function() {
 }
 
 User.prototype.requestNewPhoto = function() {
-    console.log(` New photo request was sent for ${this.name}`);
+    console.log(`New photo request was sent for ${this.name}`);
 }
 
 User.prototype.setAge = function(age) {
-    if (age >= 25) {
-        this.age = age;
-        console.log(` New photo request was sent for ${this.name}`)
-        return this.age;
-    }
+
     if (age <= 0) {
         console.log('error');
         return false
     }
+    this.age = age;
+    if (age >= 25) {
+        console.log(` New photo request was sent for ${this.name}`)
+    }
+    return age;
 }
 
 
@@ -30,4 +31,4 @@ console.log(user1)
 user1.setAge(0)
 
 user1.requestNewPhoto()
-console.log(user1)
+console.log(user1);
