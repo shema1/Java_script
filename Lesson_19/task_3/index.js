8952cd8039b5ddfd9abafbda5a0c14f35646845a
unknown <shymechko.m.a@gmail.com>
2019-11-26 19:09:21 +0200

    commit

--- Lesson_19/task_3/index.js
+++ Lesson_19/task_3/index.js
@@ -1,28 +1,14 @@
-function User(name, age) {
-    this.name = name;
-    this.age = age;
-}
-
-User.prototype.sayHi = function() {
-    console.log(`Hi, I am ${this.name}`);
-}
-
-User.prototype.requestNewPhoto = function() {
-    console.log(` New photo request was sent for${this.name}`);
-}
-
-User.prototype.setAge = function(age) {
-    if (age >= 25) {
-        this.age = age;
-        console.log(` New photo request was sent for ${this.name}`)
-        return this.age;
-    }
-    if (age <= 0) {
-        return false
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    },
+    set fullName(value) {
+        let [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
 
-
-let user1 = new User('tom', 22);
-
-console.log(user1)
+export default user;
