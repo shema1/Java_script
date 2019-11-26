c8edfcd5c7eba1da172a0ba92bee2b4479ae6370
unknown <shymechko.m.a@gmail.com>
2019-11-26 13:27:25 +0200

    commit

--- Lesson_18/hw_1/index.js
+++ Lesson_18/hw_1/index.js
@@ -1,18 +1,32 @@
-const user = {
-    firstName: '',
-    lastName: '',
-    getFullName() {
-        console.log(`${this.firstName} ${this.lastName}`)
-        return `${this.firstName} ${this.lastName}`
-    },
-    setFullName(fullName) {
-        let array = fullName.split(' ');
-        this.firstName = array[0];
-        this.lastName = array[1];
-    }
+function test(a, b) {
+    return Math.sqrt(a * a + b * b);
 }
 
-user.getFullName();
-// user.setFullName('Tom Bom')
-// user.getFullName();
-export { user };
+function spy(func) {
+    function reg(...arg) {
+        reg.calls.push(arg)
+        return func.apply(this, arg);
+    }
+
+    reg.calls = []
+    return reg
+}
+
+
+
+const spyTest = spy(test);
+spyTest(4, 2);
+spyTest(9, 1);
+
+console.log(spyTest.calls)
+
+
+// const user = {
+//     name: 'John',
+//     sayHi() {
+//         return this.name;
+//     }
+// };
+// const spyMethod = spy(user.sayHi);
+// spyMethod.apply({ name: 'Tom' }); // 'Tom'
+// console.log(spyMethod.calls) // [ [] ]
