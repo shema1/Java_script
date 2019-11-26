d8bc30b82c1759dcb8e046652a88d408afa740cb
unknown <shymechko.m.a@gmail.com>
2019-11-26 21:08:17 +0200

    commit

--- Lesson_20/task_3/index.js
+++ Lesson_20/task_3/index.js
@@ -1,14 +1,27 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`
-    },
-    set fullName(value) {
-        let [firstName, lastName] = value.split(' ');
-        this.firstName = firstName;
-        this.lastName = lastName;
+class Wallet {
+    constructor(count) {
+        this._count = count;
+    }
+    getBalance() {
+        return `${this._count}`
+    }
+
+    deposit(value) {
+        return this._count += value;
+    }
+
+    withdraw(value) {
+        if (this._count - value <= 0) {
+            console.log('No enough funds')
+            return;
+        }
+        return this._count -= value
     }
 }
 
-export default user;
+export { Wallet }
+
+// let user = new Wallet(500);
+// console.log(user.deposit(100))
+// console.log(user.withdraw(150))
+// console.log(user.getBalance())
