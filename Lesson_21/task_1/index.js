67d140b20969181d95df622883f527b10271b0cb
unknown <shymechko.m.a@gmail.com>
2019-11-27 18:20:36 +0200

    commit

--- Lesson_21/task_1/index.js
+++ Lesson_21/task_1/index.js
@@ -1,8 +1,8 @@
 const task = [
     { text: 'Buy milk', done: false },
     { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: true },
-    { text: 'Visit doctor', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
     { text: 'Buy meat', done: true },
 ]
 
