453519559f4d6b39279d147d31757252d9482e7f
unknown <shymechko.m.a@gmail.com>
2019-12-02 14:52:33 +0200

    commit

---
+++ Lesson_24/hw1/index.js
@@ -0,0 +1,29 @@
+const remainder = (firstArg, secondArg) => {
+    return Math.ceil(firstArg - secondArg);
+}
+
+
+function getDiff(startDate, endDate) {
+    const d = 1000 * 60 * 60 * 24;
+    const h = 1000 * 60 * 60;
+    const m = 1000 * 60;
+
+
+    const diff = endDate - startDate;
+    const day = Math.ceil(diff / d);
+
+
+    const daysInMs = day * d;
+    const diffDays = diff - daysInMs;
+    const hours = Math.ceil(diffDays / (h));
+
+
+    const minInMs = hours * h
+    const diffHour = diffDays - minInMs;
+    const min = Math.ceil(diffHour / (m));
+
+    const secInMs = (min * m) / 1000;
+    const sec = Math.ceil(diffHour - secInMs);
+
+    return `d${day} h${hours} m${min} s${sec}`
+}
