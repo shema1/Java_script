2c547b0c1ba47c45b2ba2b7eb1db7e7d3edb0fad
unknown <shymechko.m.a@gmail.com>
2019-12-03 10:12:35 +0200

    commit

--- Lesson_24/hw2/index.js
+++ Lesson_24/hw2/index.js
@@ -14,7 +14,27 @@ let student = [{
         name: 'Oleh',
         birthDate: '12/18/2010'
     },
+    {
+        name: 'Igor',
+        birthDate: '12/18/2010'
+    }
 ];
 
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+function studentsBirthDays(students) {
+
+    let obj = {}
+    student.sort((a, b) => new Date(a.birthDate) - new Date(b.birthDate))
+        .map(elem => {
+            if (obj[months[new Date(elem.birthDate).getMonth()]] === undefined) {
+                obj[months[new Date(elem.birthDate).getMonth()]] = [];
+            }
+            obj[months[new Date(elem.birthDate).getMonth()]].push(elem.name)
+        });
+    return obj
+}
+
+// console.log(studentsBirthDays(student))
+export { student }
