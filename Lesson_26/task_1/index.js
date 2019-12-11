2720d1b087aad3217451def3ebb71dddaf437219
unknown <shymechko.m.a@gmail.com>
2019-12-11 19:08:50 +0200

    commit

--- Lesson_26/task_1/index.js
+++ Lesson_26/task_1/index.js
@@ -2,7 +2,7 @@ localStorage.clear()
 localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }))
 localStorage.setItem('name', JSON.stringify('Tom'))
 localStorage.setItem('age', JSON.stringify(17))
-const getLocalStorageData = () => {
+export const getLocalStorageData = () => {
     return Object.entries(localStorage)
         .reduce((acc, [key, value]) => {
             let newValue;
