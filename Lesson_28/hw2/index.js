acccbc40cd675dc5b67c344d866dd6dac3fee14c
unknown <shymechko.m.a@gmail.com>
2019-12-16 19:50:42 +0200

    commit

--- Lesson_28/hw2/index.js
+++ Lesson_28/hw2/index.js
@@ -1,11 +1,10 @@
- const markFavorites = (tree, favorites) => {
-     const isFavorite = favorites.includes(tree.id);
-
-     return {
-         ...tree,
-         isFavorite,
-         nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
-     }
- };
-
- export { markFavorites }
+export const maxFibonacci = (num) => {
+    let first = 1;
+    let second = 1;
+    for (let i = 0; i <= num; i++) {
+        let numFib = first + second;
+        first = second;
+        second = numFib;
+        if (numFib > num) return first
+    }
+}
