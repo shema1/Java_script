cce989159140db6d84b313273092c87a940b3dc2
unknown <shymechko.m.a@gmail.com>
2019-12-17 10:13:59 +0200

    cpmmit

--- Lesson_29/hw1/index.js
+++ Lesson_29/hw1/index.js
@@ -2,16 +2,18 @@ export const requestUserData = (userId, callback) => {
 
 
     setTimeout(() => {
-        if (userId == 'broken') {
-            callback(null, 'Failed to load user data')
-            return
-        }
+            if (userId == 'broken') {
+                callback(null, 'Failed to load user data')
+                return
+            } else {
+                callback({
+                    name: 'John',
+                    age: 17,
+                    userId: 'userid777',
+                    email: 'userid777@example.com'
+                })
+            }
+        },
 
-        callback({
-            name: 'John',
-            age: 17,
-            userId: 'userid777',
-            email: 'userid777@example.com'
-        })
-    }, Math.floor(Math.random() * 3 + 1) * 1000);
+        Math.floor(Math.random() * 3 + 1) * 1000);
 }
