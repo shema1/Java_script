815dc920a29e437db6aa7af43b1edaffabd7891b
unknown <shymechko.m.a@gmail.com>
2019-12-17 11:25:47 +0200

    commit

--- Lesson_29/hw2/index.js
+++ Lesson_29/hw2/index.js
@@ -1,5 +1,5 @@
 export const delay = (delay, callback, context, ...args) => {
     setTimeout(() => {
-        callback.apply(context, ...args);
+        callback.bind(context, ...args);
     }, delay);
 }
