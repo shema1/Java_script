e9af5612dfca4f17c2a4dd635d0325baf2cbbc21
unknown <shymechko.m.a@gmail.com>
2019-12-17 18:12:40 +0200

    commit

--- Lesson_30/hw1/index.js
+++ Lesson_30/hw1/index.js
@@ -1,18 +1,10 @@
-export const requestUserData = (userId, callback) => {
+export const delay = time => {
 
+    let promise = new Promise((resolve, reject) => {
 
-    const onLoad = () => {
-        if (userId == 'broken') {
-            callback(null, 'Failed to load user data');
-            return;
-        }
-        callback({
-            name: 'John',
-            age: 17,
-            userId,
-            email: `${userId}@example.com`,
-        });
-    }
-    setTimeout(onLoad, Math.floor(Math.random() * 3 + 1) * 1000);
-
+        setTimeout(() => {
+            console.log('Done')
+        }, time)
+    })
+    return promise
 }
