e611830bf872d7568dfe0836c0355245f4741ae2
unknown <shymechko.m.a@gmail.com>
2019-12-17 10:55:23 +0200

    commit

--- Lesson_29/hw1/index.js
+++ Lesson_29/hw1/index.js
@@ -3,15 +3,15 @@ export const requestUserData = (userId, callback) => {
 
     const onLoad = () => {
         if (userId == 'broken') {
-            callback(null, 'Failed to load user data')
-            return
+            callback(null, 'Failed to load user data');
+            return;
         }
         callback({
             name: 'John',
             age: 17,
-            userId: `${userid777}`,
-            email: `${userid777}@example.com`
-        })
+            userId,
+            email: `${userId}@example.com`,
+        });
     }
     setTimeout(onLoad, Math.floor(Math.random() * 3 + 1) * 1000);
 
