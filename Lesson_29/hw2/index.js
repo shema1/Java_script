522d8391357bf200c005fd0c2a02b42b40e7e8ff
unknown <shymechko.m.a@gmail.com>
2019-12-17 11:22:15 +0200

    commit

--- Lesson_29/hw2/index.js
+++ Lesson_29/hw2/index.js
@@ -1,14 +1,5 @@
-export const maxFibonacci = (num) => {
-    if (num < 1) return 0;
-    let first = 1;
-    let second = 1;
-    for (let i = 0; i <= num; i++) {
-        let numFib = first + second;
-        first = second;
-        second = numFib;
-        if (numFib > num) return first
-        if (numFib == num) return second
-    }
+export const delay = (delay, callback, context, ...args) => {
+    setTimeout(() => {
+        callback.apply(context, ...args);
+    }, delay);
 }
-
-// maxFibonacci(15)
