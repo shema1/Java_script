857874b55af9f84d5577f0dc0f7bc60d0da2fadd
unknown <shymechko.m.a@gmail.com>
2019-12-20 18:14:04 +0200

    commit

--- Lesson_33/hw1/index.js
+++ Lesson_33/hw1/index.js
@@ -1,79 +1,79 @@
-  const userAvatarElem = document.querySelector('.user__avatar');
-  const userNameElem = document.querySelector('.user__name');
-  const userEmailElem = document.querySelector('.user__email');
-  const userCommitElem = document.querySelector('.user__commit');
+//   const userAvatarElem = document.querySelector('.user__avatar');
+//   const userNameElem = document.querySelector('.user__name');
+//   const userEmailElem = document.querySelector('.user__email');
+//   const userCommitElem = document.querySelector('.user__commit');
 
-  const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 
-  userAvatarElem.src = defaultAvatar;
+userAvatarElem.src = defaultAvatar;
 
-  const getMostActiveDevs = (days, userName) => {
-      return fetch(`https://api.github.com/repos/${userName}`)
-          .then(respons => respons.json())
-          .then(respons => filterDevByDate(respons, days))
-          .then(respons => sumDevCommit(respons))
+const getMostActiveDevs = (days, userName) => {
+    return fetch(`https://api.github.com/repos/${userName}`)
+        .then(respons => respons.json())
+        .then(respons => filterDevByDate(respons, days))
+        .then(respons => sumDevCommit(respons))
 
-  };
+};
 
-  const dayInMs = day => {
-      let ms = day * 86400000
-      return ms
-  };
+const dayInMs = day => {
+    let ms = day * 86400000
+    return ms
+};
 
-  const findDev = (arr, name) => {
-      let findElem = arr.find(elem => elem.name == name)
-      return findElem
-  }
+const findDev = (arr, name) => {
+    let findElem = arr.find(elem => elem.name == name)
+    return findElem
+}
 
-  const sumDevCommit = obj => {
-      let arr = []
-      obj.map(elem => {
-          let currentDev = findDev(arr, elem.name)
-          if (currentDev == undefined) {
-              arr.push(elem)
-              return
-          }
-          if (elem.name === currentDev.name) {
-              currentDev.count++;
-          }
-      })
+const sumDevCommit = obj => {
+    let arr = []
+    obj.map(elem => {
+        let currentDev = findDev(arr, elem.name)
+        if (currentDev == undefined) {
+            arr.push(elem)
+            return
+        }
+        if (elem.name === currentDev.name) {
+            currentDev.count++;
+        }
+    })
 
-      return arr.sort((a, b) => b.count - a.count)[0]
-  }
+    return arr.sort((a, b) => b.count - a.count)[0]
+}
 
-  const filterDevByDate = (obj, days) => {
+const filterDevByDate = (obj, days) => {
 
-      let filterDate = new Date().getTime() - dayInMs(days)
-      let arrDev = obj.filter(date => new Date(date.commit.author.date) > filterDate)
-          .map(dev => {
-              return {
-                  count: 1,
-                  name: dev.commit.author.name,
-                  email: dev.commit.author.email,
-                  avatar: dev.author.avatar_url
-              }
-          })
+    let filterDate = new Date().getTime() - dayInMs(days)
+    let arrDev = obj.filter(date => new Date(date.commit.author.date) > filterDate)
+        .map(dev => {
+            return {
+                count: 1,
+                name: dev.commit.author.name,
+                email: dev.commit.author.email,
+                avatar: dev.author.avatar_url
+            }
+        })
 
-      return arrDev
-  }
+    return arrDev
+}
 
 
-  const renderUserData = userData => {
-      const { count, name, email, avatar } = userData;
+const renderUserData = userData => {
+    const { count, name, email, avatar } = userData;
 
-      userAvatarElem.src = avatar;
-      userCommitElem.textContent = `${ "commits: " + count }`;
-      userNameElem.textContent = `${"name: "+ name}|`;
-      userEmailElem.textContent = `${'email: ' + email}|`
-  };
+    userAvatarElem.src = avatar;
+    userCommitElem.textContent = `${ "commits: " + count }`;
+    userNameElem.textContent = `${"name: "+ name}|`;
+    userEmailElem.textContent = `${'email: ' + email}|`
+};
 
-  const showUserBtnElem = document.querySelector('.name-form__btn');
-  const userNameInputElem = document.querySelector('.name-form__input');
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
 
-  const onSearchUser = () => {
-      const userName = userNameInputElem.value;
-      getMostActiveDevs(10, userName)
-          .then(userData => renderUserData(userData));
-  };
+const onSearchUser = () => {
+    const userName = userNameInputElem.value;
+    getMostActiveDevs(10, userName)
+        .then(userData => renderUserData(userData));
+};
 
-  showUserBtnElem.addEventListener('click', onSearchUser);
+showUserBtnElem.addEventListener('click', onSearchUser);
