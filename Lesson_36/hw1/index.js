faf280e78f6211255e40f44f18c13815ccded558
unknown <shymechko.m.a@gmail.com>
2019-12-27 14:43:10 +0200

    commit

--- Lesson_36/hw1/index.js
+++ Lesson_36/hw1/index.js
@@ -1,9 +1,25 @@
-export const parseUser = str => {
+export const getUsersBlogs = async(...userId) => {
+
     try {
-        let result = JSON.parse(str)
-        return result
-    } catch (error) {
-        console.log(error.name)
-        return null
+        const wraper = userId;
+        let test = []
+        for (let index = 0; index < wraper.length; index++) {
+            let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
+            let userData = await (response.json())
+            test.push(userData.blog)
+        }
+
+        // wraper.map(elem => {
+        //     let response = await fetch(`https://api.github.com/users/${elem}`)
+        //     let userData = await (response.json())
+        //     test.push(userData.blog)
+        // })
+
+        console.log(test)
+    } catch (e) {
+        throw new Error('Failed')
     }
 }
+
+
+getUsersBlogs("facebook", "shema1", "google");
