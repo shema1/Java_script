93727cddd7edf2a60519b73e07630bfb5566615a
unknown <shymechko.m.a@gmail.com>
2019-12-30 00:56:52 +0200

    commit

--- Lesson_36/hw1/index.js
+++ Lesson_36/hw1/index.js
@@ -1,25 +1,18 @@
-export const getUsersBlogs = async(...userId) => {
-
-    try {
-        const wraper = userId;
-        let test = []
-        for (let index = 0; index < wraper.length; index++) {
-            let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
-            let userData = await (response.json())
-            test.push(userData.blog)
-        }
-
-        // wraper.map(elem => {
-        //     let response = await fetch(`https://api.github.com/users/${elem}`)
-        //     let userData = await (response.json())
-        //     test.push(userData.blog)
-        // })
-
-        console.log(test)
-    } catch (e) {
-        throw new Error('Failed')
-    }
-}
+ export const getUsersBlogs = async(...userId) => {
 
 
-getUsersBlogs("facebook", "shema1", "google");
+     const wraper = userId;
+     let test = []
+     for (let index = 0; index < wraper.length; index++) {
+         let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
+         if (!response.ok) throw new Error('Failed to load data');
+         let userData = await (response.json())
+         test.push(userData.blog)
+     }
+
+     return test
+ }
+
+
+ getUsersBlogs('google', "facebook", 'shema1')
+     .then(response => console.log(response));
