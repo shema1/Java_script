bb4e4ddbc41a24cc9f84fbe9612546144e1b6209
unknown <shymechko.m.a@gmail.com>
2019-12-30 01:01:05 +0200

    commit

--- Lesson_36/hw1/index.js
+++ Lesson_36/hw1/index.js
@@ -1,18 +1,18 @@
- export const getUsersBlogs = async(...userId) => {
+const getUsersBlogs = async(...userId) => {
 
 
-     const wraper = userId;
-     let test = []
-     for (let index = 0; index < wraper.length; index++) {
-         let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
-         if (!response.ok) throw new Error('Failed to load data');
-         let userData = await (response.json())
-         test.push(userData.blog)
-     }
+    const wraper = userId;
+    let test = []
+    for (let index = 0; index < wraper.length; index++) {
+        let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
+        if (!response.ok) throw new Error('Failed to load data');
+        let userData = await (response.json())
+        test.push(userData.blog)
+    }
 
-     return test
- }
+    return test
+}
 
-
- getUsersBlogs(['google', "facebook", 'shema1'])
-     .then(response => console.log(response));
+export { getUsersBlogs }
+// getUsersBlogs('google', "facebook", 'shema1')
+//     .then(response => console.log(response));
