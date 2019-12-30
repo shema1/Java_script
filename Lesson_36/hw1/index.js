1d5bff32b19cf2b25561b005ad0f3718723e6447
unknown <shymechko.m.a@gmail.com>
2019-12-30 10:00:20 +0200

    commit

--- Lesson_36/hw1/index.js
+++ Lesson_36/hw1/index.js
@@ -1,23 +1,26 @@
-export const getUsersBlogs = async(userId) => {
+ export const getUsersBlogs = async(...userId) => {
+
+     try {
+         const wraper = userId;
+         let test = []
+         for (let index = 0; index < wraper.length; index++) {
+             let response = await fetch(`https://api.github.com/users/${wraper[index]}`)
+             if (!response.ok) throw new Error('Failed to load data');
+             let userData = await (response.json())
+             test.push(userData.blog)
+         }
+
+         // wraper.map(elem => {
+         //     let response = await fetch(`https://api.github.com/users/${elem}`)
+         //     let userData = await (response.json())
+         //     test.push(userData.blog)
+         // })
+         return test
+             //  console.log(test)
+     } catch (e) {
+         throw new Error('Failed')
+     }
+ }
 
 
-    // const wraper = userId;
-    let test = []
-    for (let index = 0; index < userId.length; index++) {
-        // try {
-
-        let response = await fetch(`https://api.github.com/users/${userId[index]}`)
-        if (!response.ok) throw new Error('Failed to load data');
-        let userData = await (response.json())
-        test.push(userData.blog)
-            // } catch (e) {
-            //     console.log(e.message)
-            // }
-    }
-
-    return test
-}
-
-// export { getUsersBlogs }
-// getUsersBlogs('google', "facebook", 'shema1')
-//     .then(response => console.log(response));
+ getUsersBlogs("facebook");
