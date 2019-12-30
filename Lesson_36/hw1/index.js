a84e77dfcc27cae7fc0fe58c5bdd2f0737294749
unknown <shymechko.m.a@gmail.com>
2019-12-30 10:05:36 +0200

    commit

--- Lesson_36/hw1/index.js
+++ Lesson_36/hw1/index.js
@@ -1,28 +1,28 @@
- export const getUsersBlogs = async(userId) => {
+  const getUsersBlogs = async(userId) => {
 
-     let test = []
-     try {
-         //  const wraper = userId;
-         for (let index = 0; index < wraper.length; index++) {
-             let response = await fetch(`https://api.github.com/users/${userId[index]}`)
-             if (!response.ok) throw new Error('Failed to load data');
-             let userData = await (response.json())
-             test.push(userData.blog)
-         }
+      let test = []
+      try {
+          //  const wraper = userId;
+          for (let index = 0; index < userId.length; index++) {
+              let response = await fetch(`https://api.github.com/users/${userId[index]}`)
+              if (!response.ok) throw new Error('Failed to load data');
+              let userData = await (response.json())
+              test.push(userData.blog)
+          }
 
-         // wraper.map(elem => {
-         //     let response = await fetch(`https://api.github.com/users/${elem}`)
-         //     let userData = await (response.json())
-         //     test.push(userData.blog)
-         // })
-         return test
-             //  console.log(test)
-     } catch (e) {
-         //  throw new Error('Failed')
-         console.log(e)
-     }
- }
+          // wraper.map(elem => {
+          //     let response = await fetch(`https://api.github.com/users/${elem}`)
+          //     let userData = await (response.json())
+          //     test.push(userData.blog)
+          // })
+          return test
+              //  console.log(test)
+      } catch (e) {
+          //  throw new Error('Failed')
+          console.log(e)
+      }
+  }
 
 
- getUsersBlogs(["facebook", "google"])
-     .then(response => console.log(response))
+  getUsersBlogs(["facebook", "google"])
+      .then(response => console.log(response))
